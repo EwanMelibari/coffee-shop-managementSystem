0d60302968349dbaa5b2190c9fcc9154ee936c5e
Ewan Melibari <ewanhasan2004@gmail.com>
2025-05-12 18:26:21 +0300

    New Class addedd and service methods

--- src/main/java/com/example/coffee_shop_managementSystem/model/service/admin/ManageEmployeeService.java
+++ src/main/java/com/example/coffee_shop_managementSystem/model/service/admin/ManageEmployeeService.java
@@ -1,10 +1,40 @@
 package com.example.coffee_shop_managementSystem.model.service.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.coffee_shop_managementSystem.model.entity.User;
+import com.example.coffee_shop_managementSystem.model.repository.UserRepository;
 
 @Service
 public class ManageEmployeeService {
 
-    
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveEmployee(User user){
+        userRepository.save(user);
+
+    }
+
+    public void deleteEmployee(Integer id){
+        userRepository.deleteById(id);
+    }
+
+    public List<User> getAllEmployees(){
+        return userRepository.findAll();
+    }
+
+    public void updateEmployee(User user){
+        if(userRepository.existsById(user.getId())){
+            userRepository.save(user);
+        }
+    }
+
+    public User getEmployeeById(Integer id){
+        return userRepository.findById(id).orElse(null);
+    }
     
 }
--- src/main/java/com/example/coffee_shop_managementSystem/model/service/admin/ManageProductsService.java
+++ src/main/java/com/example/coffee_shop_managementSystem/model/service/admin/ManageProductsService.java
@@ -1,8 +1,39 @@
 package com.example.coffee_shop_managementSystem.model.service.admin;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.coffee_shop_managementSystem.model.entity.Product;
+import com.example.coffee_shop_managementSystem.model.repository.ProductRepository;
 
 @Service
 public class ManageProductsService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    public void deleteProduct(Integer id) {
+        productRepository.deleteById(id);
+    }
+
+    public Product getProductById(Integer id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public void updateProduct(Product product) {
+       if(productRepository.existsById(product.getProductId())){
+           productRepository.save(product);
+       }
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
     
 }
